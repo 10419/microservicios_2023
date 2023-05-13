47f3d0d88f1e66cd4950365ceccff990824454f9
10419 <martinhilasaca7@gmail.com>
2023-05-12 22:31:25 -0600

    tarea 2

--- cliente/src/main/java/com/example/cliente/ClienteApplication.java
+++ cliente/src/main/java/com/example/cliente/ClienteApplication.java
@@ -17,9 +17,9 @@ public class ClienteApplication {
 	@Bean
 	public OpenAPI custoOpenAPI(){
 		return new OpenAPI().info(new Info()
-				.title("open api microservicios catalofo")
+				.title("open api microservicios cliente")
 				.version("0.0.1")
-				.description("servicio web catalogo")
+				.description("servicio web cliente")
 				.termsOfService("http://swagger.io/terms")
 				.license(new License().name("Apache 2.0").url("http://springdoc.org"))
 		);
